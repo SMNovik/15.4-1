14cefbfe12e5004620b999a1326dbc19c643faa2
Sergey Novik <110816135+SMNovik@users.noreply.github.com>
2022-09-24 15:41:22 +0300

    Add files via upload

---
+++ через словарь нахождение частоты слова в тексте и самого длинного.py
@@ -0,0 +1,32 @@
+﻿alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+name_f = input('Путь к файлу:')
+# name_f = r'F:\ОБУЧЕНИЕ ATI\курс QA\модуль 15\yesterday.txt'
+with open(name_f, encoding='utf-8-sig') as f:
+    text = f.read()
+    text = text.lower()
+    for i in '!"\'#$%&()*+-,/:;<=>?@[\\]^_{|}~':
+        text = text.replace(i, '')
+
+    text = text.split()
+# print('text', text) # выводит текст в виде списка(после split)
+
+count_freg = {}  # словарь - слово :максимальная частота
+freg = 1  # счетчик частоты слова в тексте
+len_word = 1  # счетчик длинны слова
+engl_word = {}  # словарь - английское слово:его длинна
+for word in text:
+    if text.count(word) > freg and len(word) > 3:  # сравниваем часто слов в тексте
+        freg = text.count(word)  # присваиваем счетчику частоту более часто встречающегося слова
+        count_freg = {word: freg}  # словарь - слово:частота
+    elif text.count(word) == freg and len(word) > 3:  # ищем слова с такой же частотой и длинной больше 3
+        count_freg[word] = text.count(word)  # добавляем новое слово такой же частоты в словарь
+    if word[0] in alphabet:  # проверка первой буквы слова
+        if len(word) > len_word:  # сравниваем длинну слов
+            len_word = len(word)  # присваиваем счетчику значение более длинного слова
+            engl_word = {word: len(word)}  # переприсваиваем новый словарь с более длинным словом
+        elif len(word) == len_word:
+            engl_word[word] = len(word)  # добавляем в словарь слово такой же длинны
+for j in count_freg:
+    print(f'Наиболее часто встречающееся слово/слова в тексте: {j}. Частота в тексте: {count_freg[j]}')
+for i in engl_word:
+    print(f'Наиболее длинное английское слово/слова в тексте :{i}. Его длинна: {engl_word[i]}')
---
+++ через список(массив) нахождение частоты слова в тексте и самого длинного.py
@@ -0,0 +1,65 @@
+﻿def changeText(text):
+    """
+    Функция принимает строку с текстом.
+    Убирает все знаки препинания и возвращает
+    список, состоящий из слов текста.
+    """
+    for i in '!"\'#$%&()*+-,/:;<=>?@[\\]^_{|}~':
+        text = text.replace(i, '')
+
+    return text.split()
+
+def mostCommon(text, length=0):
+    """
+    Функция принимает список и ограничение по длине.
+    Возвращает самый часто встречающийся элемент.
+    Если есть несколько элементов с одинаковой самой большой частотой, то вернёт их все.
+    """
+    most_common = []      # список слов чаще всего встречаемыхв тексте
+    qty_most_common = 0   # сколько раз встречается слово в тексте
+
+    for item in text:
+        if len(item) > length:
+            qty = text.count(item)  # счетчик сколько раз встречается слово в тексте
+            if qty > qty_most_common and qty > 2:
+                qty_most_common = qty  # присвается новое значение счетчику количества
+                most_common = [item]
+            elif qty == qty_most_common:
+                most_common.append(item)
+
+    return list(set(most_common))
+
+def mostLength(text):
+    """
+    Функция принимает список.
+    Возвращает самый длинный элемент.
+    Если есть несколько элементов с одинаковой самой большой длиной, то вернёт их все.
+    """
+    most_length = []
+    qty_most_length = 0
+    alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    for item in text:
+        for char in item:
+            if char not in alphabet:
+                charEn = False
+            else:
+                charEn = True
+
+        if charEn:
+            qty = len(item)
+            if qty > qty_most_length:
+                qty_most_length = qty
+                most_length = [item]
+            elif qty == qty_most_length:
+                most_length.append(item)
+
+    return list(set(most_length))
+
+# nameFile = input('Название файла: ')
+nameFile = r'F:\ОБУЧЕНИЕ ATI\курс QA\модуль 15\yesterday.txt'
+with open(nameFile, encoding='utf-8-sig') as f:
+    fileText = f.read()
+
+fileText = changeText(fileText)
+print(f'Список самых частых слов длинной более трёх символов: {mostCommon(fileText, 2)}')
+print(f'Список самых длинных английских слов: {mostLength(fileText)}')
